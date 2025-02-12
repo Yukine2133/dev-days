a9665c34942be0c741a3e55269403533f8d94b37
Yukine213 <kondratenkov1109@gmail.com>
2025-02-12 14:55:50 +0200

    fix the position of the button when error appears

--- components/RepositoryInput.tsx
+++ components/RepositoryInput.tsx
@@ -37,23 +37,25 @@ const RepositoryInput = ({
 
   return (
     <form
-      className="flex mb-6 w-full max-w-7xl  flex-col  justify-center relative"
+      className="flex mb-6 w-full max-w-7xl  flex-col  justify-center "
       onSubmit={handleSubmit}
     >
-      <Input
-        className="w-full rounded-lg h-12 bg-zinc-800/50 px-4 py-3 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
-        type="text"
-        placeholder="Enter a repository link"
-        value={repoUrl}
-        onChange={(e) => setRepoUrl(e.target.value)}
-      />
+      <div className="relative">
+        <Input
+          className="w-full rounded-lg h-12 bg-zinc-800/50 px-4 py-3 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          type="text"
+          placeholder="Enter a repository link"
+          value={repoUrl}
+          onChange={(e) => setRepoUrl(e.target.value)}
+        />
+        <Button
+          className="absolute right-2 top-1/2 h-8 -translate-y-1/2 rounded-md bg-purple-600 px-4 py-1.5 text-sm font-medium text-white hover:bg-purple-700"
+          type="submit"
+        >
+          Search
+        </Button>
+      </div>
       {error && <p className="text-red-500">{error}</p>}
-      <Button
-        className="absolute right-2 top-1/2 h-8 -translate-y-1/2 rounded-md bg-purple-600 px-4 py-1.5 text-sm font-medium text-white hover:bg-purple-700"
-        type="submit"
-      >
-        Search
-      </Button>
     </form>
   );
 };
