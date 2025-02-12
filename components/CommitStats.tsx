dd7f990c93b82f9ccc8361e663b27c528593d2cd
Yukine213 <kondratenkov1109@gmail.com>
2025-02-12 13:46:35 +0200

    feat: improve ui

--- components/CommitStats.tsx
+++ components/CommitStats.tsx
@@ -18,43 +18,45 @@ const CommitStats = ({ commits }: CommitStatsProps) => {
   } = useCommitStats(commits);
 
   return (
-    <div>
-      <h2 className="text-xl mt-4">
-        You have been working on this project for:{" "}
-        <span className="font-bold">{uniqueCommitDays} days</span>
-      </h2>
+    <div className="mt-8 ">
+      <div className="flex gap-4 items-center">
+        <div className="border  border-stone-700">
+          <h2 className="text-xl mt-4 border-b border-stone-700 px-4 pb-2">
+            You have been working on this project for:{" "}
+            <span className="font-bold">{uniqueCommitDays} days</span>
+          </h2>
+          {formattedMaxCommitDate && (
+            <h2 className="text-xl mt-4 border-b border-stone-700 px-4 pb-2">
+              On <span className="font-bold">{formattedMaxCommitDate}</span>,
+              you had the most commits:{" "}
+              <span className="font-bold">{maxCommits}</span>.
+            </h2>
+          )}
+          <h2 className="text-xl mt-4 border-b border-stone-700 px-4 pb-2">
+            Your longest commit streak is:{" "}
+            <span className="font-bold">{longestStreak} days</span>.
+          </h2>
+        </div>
+        <div className="border border-stone-700">
+          {getMostActiveHours()}
+          <h2 className="text-xl mt-4 border-b border-stone-700 px-4 pb-2">
+            You commit the most in the{" "}
+            <span className="font-bold">
+              {timeLabels[mostActiveTimePeriod.period]}
+            </span>
+            .
+          </h2>
+          <h2 className="text-xl mt-4 border-b border-stone-700 px-4 pb-2">
+            Your least active time is the{" "}
+            <span className="font-bold">
+              {timeLabels[leastActiveTimePeriod.period]}
+            </span>
+            .
+          </h2>
+        </div>
+      </div>
 
-      {formattedMaxCommitDate && (
-        <h2 className="text-xl mt-4">
-          On <span className="font-bold">{formattedMaxCommitDate}</span>, you
-          had the most commits: <span className="font-bold">{maxCommits}</span>.
-        </h2>
-      )}
-
-      <h2 className="text-xl mt-4">
-        Your longest commit streak is:{" "}
-        <span className="font-bold">{longestStreak} days</span>.
-      </h2>
-
-      {getMostActiveHours()}
-
-      <h2 className="text-xl mt-4">
-        You commit the most in the{" "}
-        <span className="font-bold">
-          {timeLabels[mostActiveTimePeriod.period]}
-        </span>
-        .
-      </h2>
-
-      <h2 className="text-xl mt-4">
-        Your least active time is the{" "}
-        <span className="font-bold">
-          {timeLabels[leastActiveTimePeriod.period]}
-        </span>
-        .
-      </h2>
-
-      <h2 className="text-xl mt-4">
+      <h2 className="text-xl text-center mx-auto w-fit mt-4 border border-stone-700 px-4 pb-2">
         Most common commit message:{" "}
         <span className="font-bold">{mostCommonWord}</span> with{" "}
         <span className="font-bold">{maxCount}</span> occurrences
